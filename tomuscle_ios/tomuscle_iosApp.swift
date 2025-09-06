d249912e8560b974a27a8bda94ecf1b46243bd9c
hirossan4049 <haruto405329@gmail.com>
2025-09-06 22:24:50 +0900

    Initial Commit

---
+++ tomuscle_ios/tomuscle_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomuscle_iosApp.swift
+//  tomuscle_ios
+//
+//  Created by a on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct tomuscle_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
